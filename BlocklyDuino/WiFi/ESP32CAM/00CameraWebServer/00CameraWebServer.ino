4ee0b6489c7f44355bd72aee4232c3923255e713
AMANI <kingofamani@gmail.com>
2020-12-16 13:10:00 +0800

    修改：esp32cam加入HTCAMANI WIFI

--- BlocklyDuino/WiFi/ESP32CAM/00CameraWebServer/00CameraWebServer.ino
+++ BlocklyDuino/WiFi/ESP32CAM/00CameraWebServer/00CameraWebServer.ino
@@ -15,8 +15,10 @@
 
 #include "camera_pins.h"
 
-const char* ssid = "AMANI-4G-Home";
-const char* password = "c41585c41585";
+const char* ssid = "HTCAMANI";
+const char* password = "12345678";
+//const char* ssid = "AMANI-4G-Home";
+//const char* password = "c41585c41585";
 //const char* ssid = "deyin-305ap";
 //const char* password = "22924152";
 
