8325a60b7af53bd1edbd935896bb59f5ff5b3dc0
AMANI <kingofamani@gmail.com>
2021-01-18 14:08:40 +0800

    修改：[BUG]步進馬達圈數控制

--- BlocklyDuino/18StepMotor/18StepMotor.ino
+++ BlocklyDuino/18StepMotor/18StepMotor.ino
@@ -3,7 +3,7 @@
  *
  * https://github.com/MediaTek-Labs/BlocklyDuino-for-LinkIt
  *
- * Date: Thu, 29 Oct 2020 07:19:19 GMT
+ * Date: Mon, 18 Jan 2021 06:06:25 GMT
  */
 
 #include <Stepper.h>
@@ -17,17 +17,24 @@ void setup()
 {
   Serial.begin(9600);
 
-  myStepper.setSpeed(13);
+  myStepper.setSpeed(12);
 }
 
 
 void loop()
 {
-  myStepper.step(steps);
-  //for (i = 0; i <= 100; i++) {
-    //myStepper.step(steps);
-  //}
-  //delay(500);
-  //myStepper.step(-steps);
-  //delay(500);
+  /*
+  ★接腳依序8,9,10,11，但程式要寫成8,10,9,11
+*/
+
+/*
+    轉1圈(0-7共8次，8*256=2048)
+  */
+  /*
+    若要轉2圈則迴圈改成0-15
+  */
+  for (i = 0; i <= 7; i++) {
+    myStepper.step(256);
+  }
+  delay(1000);
 }
