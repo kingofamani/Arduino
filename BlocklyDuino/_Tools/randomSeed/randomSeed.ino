74c665bdeb1155d2ebeac927c96dcc6091771b46
AMANI <kingofamani@gmail.com>
2022-03-14 13:50:06 +0800

    新增：自訂工具(1)亂數種子。(2)序列埠輸入字串。

--- BlocklyDuino/_Tools/randomSeed/randomSeed.ino
+++ BlocklyDuino/_Tools/randomSeed/randomSeed.ino
@@ -3,7 +3,7 @@
  *
  * https://github.com/MediaTek-Labs/BlocklyDuino-for-LinkIt
  *
- * Date: Thu, 10 Mar 2022 00:26:09 GMT
+ * Date: Mon, 14 Mar 2022 05:47:36 GMT
  */
 /*  部份程式由吉哥積木產生  */
 /*  https://sites.google.com/jes.mlc.edu.tw/ljj/linkit7697  */
@@ -11,15 +11,12 @@
 
 void setup()
 {
-  Serial.begin(9600);
-
 
 }
 
 
 void loop()
 {
-  if (Serial.available() == 1) {
-    Serial.println((Serial.readString()));
-  }
+  randomSeed(analogRead(0));
+
 }
