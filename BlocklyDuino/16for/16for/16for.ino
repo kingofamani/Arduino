5be6d8750799686fd12d84a55461413250aabe26
AMANI <kingofamani@gmail.com>
2022-03-14 21:33:40 +0800

    新增：互質範例

--- BlocklyDuino/16for/16for/16for.ino
+++ BlocklyDuino/16for/16for/16for.ino
@@ -3,39 +3,30 @@
  *
  * https://github.com/MediaTek-Labs/BlocklyDuino-for-LinkIt
  *
- * Date: Mon, 26 Oct 2020 07:37:40 GMT
+ * Date: Mon, 14 Mar 2022 12:29:05 GMT
  */
+/*  部份程式由吉哥積木產生  */
+/*  https://sites.google.com/jes.mlc.edu.tw/ljj/linkit7697  */
 
 
+int prime = true;
 
-boolean prime;
-
-int num;
-
-int i;
+int num = 11;
 
 void setup()
 {
   Serial.begin(9600);
 
-  prime = true;
-  num = 997;
-  int i_end = num - 1;
-  for (i = 2;
-      (2 <= i_end) ? i <= i_end : i >= i_end;
-      i += (2 <= i_end) ? 1 : -1) {
+  for (int i = 2; i <= num - 1; i++) {
     if (num % i == 0) {
       prime = false;
       break;
-
     }
   }
   if (prime == true) {
     Serial.println("質數");
-
   } else {
     Serial.println("不是質數");
-
   }
 }
 
--- BlocklyDuino/16for/17factor/17factor.ino
+++ BlocklyDuino/16for/17factor/17factor.ino
@@ -3,7 +3,7 @@
  *
  * https://github.com/MediaTek-Labs/BlocklyDuino-for-LinkIt
  *
- * Date: Mon, 11 Oct 2021 07:04:11 GMT
+ * Date: Mon, 14 Mar 2022 12:39:30 GMT
  */
 /*  部份程式由吉哥積木產生  */
 /*  https://sites.google.com/jes.mlc.edu.tw/ljj/linkit7697  */
@@ -11,16 +11,14 @@
 
 String num = "15";
 
-int number = (num.toInt());
-
 String factor = "";
 
 void setup()
 {
   Serial.begin(9600);
 
-  for (int i = 1; i <= number; i++) {
-    if (number % i == 0) {
+  for (int i = 1; i <= (num.toInt()); i++) {
+    if ((num.toInt()) % i == 0) {
       factor = String(factor)+String(i)+String(",");
     }
   }
