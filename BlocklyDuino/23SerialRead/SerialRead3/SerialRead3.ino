7fb1f0e2517c3b20bfba6c0cd6f073c5cc184979
AMANI <kingofamani@gmail.com>
2022-03-15 08:38:07 +0800

    新增：四則運算

--- BlocklyDuino/23SerialRead/SerialRead3/SerialRead3.ino
+++ BlocklyDuino/23SerialRead/SerialRead3/SerialRead3.ino
@@ -3,15 +3,13 @@
  *
  * https://github.com/MediaTek-Labs/BlocklyDuino-for-LinkIt
  *
- * Date: Mon, 14 Mar 2022 23:21:34 GMT
+ * Date: Mon, 14 Mar 2022 04:00:36 GMT
  */
 /*  部份程式由吉哥積木產生  */
 /*  https://sites.google.com/jes.mlc.edu.tw/ljj/linkit7697  */
 
 
-char key = ' ';
-
-String num = "";
+String B = "";
 
 void setup()
 {
@@ -24,8 +22,7 @@ void setup()
 void loop()
 {
   if (Serial.available() == 1) {
-    key = Serial.read();
-    num += key;
-    Serial.println(num);
+    B = Serial.readString();
+    Serial.println(B);
   }
 }
--- BlocklyDuino/_Tools/checkIsNumber/checkIsNumber.ino
+++ BlocklyDuino/_Tools/checkIsNumber/checkIsNumber.ino
@@ -3,29 +3,29 @@
  *
  * https://github.com/MediaTek-Labs/BlocklyDuino-for-LinkIt
  *
- * Date: Mon, 14 Mar 2022 23:21:34 GMT
+ * Date: Mon, 14 Mar 2022 23:54:07 GMT
  */
 /*  部份程式由吉哥積木產生  */
 /*  https://sites.google.com/jes.mlc.edu.tw/ljj/linkit7697  */
 
 
-char key = ' ';
-
-String num = "";
+boolean checkIsNumber(char key) {
+  char N[] = "1234567890";
+  //一定要大寫NULL
+  if ((strchr(N, key)) != (NULL)) {
+    return (true);
+  } else {
+    return (false);
+  }
+}
 
 void setup()
 {
-  Serial.begin(9600);
-
 
 }
 
 
 void loop()
 {
-  if (Serial.available() == 1) {
-    key = Serial.read();
-    num += key;
-    Serial.println(num);
-  }
+
 }
