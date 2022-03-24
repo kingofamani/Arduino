b2ad0d83227de9bb1e9ba3b62eaca89db22ed1ee
AMANI <kingofamani@gmail.com>
2022-03-24 21:58:11 +0800

    修改：1A2B，自動產生答案

--- BlocklyDuino/23SerialRead/1A2B/1A2B.ino
+++ BlocklyDuino/23SerialRead/1A2B/1A2B.ino
@@ -3,13 +3,13 @@
  *
  * https://github.com/MediaTek-Labs/BlocklyDuino-for-LinkIt
  *
- * Date: Thu, 24 Mar 2022 07:44:27 GMT
+ * Date: Thu, 24 Mar 2022 13:57:18 GMT
  */
 /*  部份程式由吉哥積木產生  */
 /*  https://sites.google.com/jes.mlc.edu.tw/ljj/linkit7697  */
 
 
-int Ans[4] = {3, 8, 2, 5};
+int Ans[4] = {-1, -1, -1, -1};
 
 int Num[4] = {0, 0, 0, 0};
 
@@ -18,6 +18,45 @@ int A = 0;
 int B = 0;
 
 int Count = 0;
+
+boolean checkIsAns0() {
+  int sum = 0;
+  for (int i = 0; i <= 3; i++) {
+    sum = sum + (Ans[i + 0]);
+  }
+  if (sum == -4) {
+    return (true);
+  } else {
+    return (false);
+  }
+}
+
+void setAns() {
+  for (int i = 0; i <= 3; i++) {
+    int n = 0;
+    if (i == 0) {
+      n = random(1, 10);
+      Ans[i] = n;
+    } else {
+      boolean isOk = false;
+      while (!(isOk == true)) {
+        n = random(0, 10);
+        int c = 0;
+        for (int j = 0; j <= 3; j++) {
+          if (n == Ans[j]) {
+            c = c + 1;
+          }
+        }
+        if (c == 0) {
+          isOk = true;
+          Ans[i] = n;
+        } else {
+          isOk = false;
+        }
+      }
+    }
+  }
+}
 
 void judgeAB() {
 }
@@ -33,6 +72,15 @@ void setup()
 
 void loop()
 {
+  if (checkIsAns0()) {
+    //亂數產生答案
+    setAns();
+    Serial.print("產生的Ans：");
+    for (int i = 0; i <= 3; i++) {
+      Serial.print((Ans[i]));
+    }
+    Serial.println("");
+  }
   if (Serial.available() == 1) {
     Count = Count + 1;
     int InputNum = (Serial.parseInt());
@@ -58,9 +106,14 @@ void loop()
     if (A == 4) {
       Serial.println((String("恭喜你答對了！答案是：")+String(InputNum)));
       Serial.println((String("你總共猜了")+String(Count)+String("次")));
-      Count = 0;
       A = 0;
       B = 0;
+      Serial.println("");
+      Serial.println("===============新的一局===============");
+      Count = 0;
+      for (int i = 0; i <= 3; i++) {
+        Ans[i] = -1;
+      }
     } else {
       Serial.println((String(InputNum)+String("：")+String(A)+String("A")+String(B)+String("B")+String("，請輸入4位數字(數字不能重複)：")+String("3825")));
       A = 0;
