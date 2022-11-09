77e02a77a27a11a4f24bac5f4ae8d5a18bc3eaba
AMANI <kingofamani@gmail.com>
2022-11-09 11:11:09 +0800

    修改：millies

--- BlocklyDuino/26millis2/26millis2.ino
+++ BlocklyDuino/26millis2/26millis2.ino
@@ -3,35 +3,52 @@
  *
  * https://github.com/MediaTek-Labs/BlocklyDuino-for-LinkIt
  *
- * Date: Tue, 08 Nov 2022 01:46:13 GMT
+ * Date: Wed, 09 Nov 2022 02:03:51 GMT
  */
 /*  部份程式由吉哥積木產生  */
 /*  https://sites.google.com/jes.mlc.edu.tw/ljj/linkit7697  */
 
 
-int secs = 0;
+int secs = 20;
 
-void startCountDown() {
+float durs = 0;
+
+void startCount() {
+  Serial.print("目前時間：");
 }
 
-int durationSecs(null, int secs) {
+float durationSecs(int item, int secs) {
   static int startTime= millis();
-  int duration = (round((millis()-startTime)/1000));
+  float duration = (round((millis()-startTime)/1000));
+  if (item == 1) {
+    //正數計時
+    Serial.print((millis()));
+    Serial.print("、");
+    Serial.print((startTime));
+    Serial.print("、");
+    Serial.print(((millis()-startTime)/1000));
+    Serial.print("、");
+    Serial.print((round((millis()-startTime)/1000)));
+    Serial.print("、");
+    Serial.println(duration);
+    return (duration);
+  } else if (item == 2) {
+    //倒數計時
+    return (secs - duration);
+  }
 }
 
 void setup()
 {
   Serial.begin(9600);
 
-
+  delay(2000);
 }
 
 
 void loop()
 {
-  Serial.println(millis());
-  secs = round(millis() / 1000);
-  Serial.print("目前時間：");
-  Serial.println(secs);
-  delay(1000);
+  durs = durationSecs(1, secs);
+  Serial.println(durs);
+  delay(500);
 }
