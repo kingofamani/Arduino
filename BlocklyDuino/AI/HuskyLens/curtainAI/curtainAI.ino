5e6fd0f2da69378a6e4e379f70a4362431d14b9b
AMANI <kingofamani@gmail.com>
2022-11-27 19:38:28 +0800

    新增：curtainAI智能窗簾

---
+++ BlocklyDuino/AI/HuskyLens/curtainAI/curtainAI.ino
@@ -0,0 +1,171 @@
+/*
+ * Generated using BlocklyDuino:
+ *
+ * https://github.com/MediaTek-Labs/BlocklyDuino-for-LinkIt
+ *
+ * Date: Sun, 27 Nov 2022 11:35:47 GMT
+ */
+/*  部份程式由吉哥積木產生  */
+/*  https://sites.google.com/jes.mlc.edu.tw/ljj/linkit7697  */
+#include "HUSKYLENS.h"
+#include <Stepper.h>
+#include <IRremote.h>
+
+HUSKYLENS huskylens;
+int recValue = 100;
+
+int countIDs[6] = {0, 0, 0, 0, 0, 0};
+
+int IDs[6] = {0, 1, 2, 3, 4, 5};
+
+int ReplyID = 0;
+
+void printResult(HUSKYLENSResult result);
+int steps=2048;
+Stepper myStepper(steps, 8, 10, 9, 11);
+boolean isStopNow = false;
+
+int maxTurns = 16;
+
+int posTurns = 16;
+
+IRrecv irrecv(3);
+decode_results results;
+void initHL() {
+  Wire.begin();
+  while (!huskylens.begin(Wire))
+  {     Serial.println(F("Begin failed!"));
+  Serial.println(F("1.Please recheck the Protocol Type in HUSKYLENS (General Settings>>Protocol Type>>I2C)"));
+  Serial.println(F("2.Please recheck the connection."));
+  delay(100);
+  }   huskylens.writeAlgorithm(ALGORITHM_OBJECT_RECOGNITION);
+}
+
+void startRecog() {
+  if (huskylens.request())
+  {
+  HUSKYLENSResult result;
+  //連續辨別成功次數
+  for (int i = 0; i < sizeof(IDs) / sizeof(IDs[0]); i++) {
+  if (huskylens.countBlocks(IDs[i]) >= 1) {
+  countIDs[IDs[i]]++;
+  }     }
+  //判斷哪個ID達到閾值
+  for (int i = 0; i < sizeof(countIDs) / sizeof(countIDs[0]); i++) {
+  if (countIDs[i] >= recValue) {
+  result = huskylens.getBlock(i, 0);
+  printResult(result);
+  //重置
+  for (int j = 0; j < sizeof(countIDs) / sizeof(countIDs[0]); j++) {
+  countIDs[j] = 0;
+  }       }     }
+  }   else   {
+  Serial.println("失敗!無法從Huskylens請求物件!");
+  }
+}
+
+void printResult(HUSKYLENSResult result) {
+if (result.command == COMMAND_RETURN_BLOCK) {
+Serial.println(result.ID);
+ReplyID = result.ID;
+}
+}
+void pR() {
+}
+
+void turnStop(float c) {
+  Serial.println((String("目前位置：")+String(posTurns)));
+  if (c > 0 && posTurns == maxTurns) {
+    isStopNow = true;
+  }
+  if (c < 0 && posTurns == 0) {
+    isStopNow = true;
+  }
+}
+
+void irrStop() {
+  if (irrecv.decode(&results)) {
+      if (String(results.value, HEX) == "ffa25d" || String(results.value, HEX) == "e318261b") {
+      isStopNow = true;
+    }
+
+    irrecv.resume();
+  }
+}
+
+void recogStop(int stopID) {
+  if (huskylens.request()) {
+    HUSKYLENSResult result;
+    if ((huskylens.countBlocks(IDs[stopID])) >= 1) {
+      isStopNow = true;
+    }
+  }
+}
+
+void go(float c) {
+  int num = ((abs(c)) * 8) - 1;
+  for (int i = 0; i <= num; i++) {
+    //是否中斷
+    turnStop(c);
+    irrStop();
+    recogStop(3);
+    if (isStopNow) {
+      isStopNow = false;
+      break;
+    }
+    //旋轉
+    if (c >= 0) {
+      posTurns = posTurns + 1;
+      myStepper.step(256);
+    } else {
+      posTurns = posTurns + -1;
+      myStepper.step(-256);
+    }
+    irrStop();
+    if (isStopNow) {
+      isStopNow = false;
+      break;
+    }
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);
+
+  irrecv.enableIRIn();
+  //打開最大轉數(1圈8轉)
+  //目前位置
+  myStepper.setSpeed(12);
+  go(-10);
+  posTurns = 0;
+  initHL();
+}
+
+
+void loop()
+{
+  //物體識別閾值，連續識別正確次數
+//訓練的ID
+//辨識的ID
+
+startRecog();
+  if (ReplyID != 0) {
+    if (ReplyID == 1) {
+      go(10);
+    } else if (ReplyID == 2) {
+      go(-10);
+    }
+    ReplyID = 0;
+  }
+  if (irrecv.decode(&results)) {
+      if (String(results.value, HEX) == "ff02fd") {
+      go(10);
+    }
+    if (String(results.value, HEX) == "ffc23d") {
+      go(-10);
+    }
+
+    irrecv.resume();
+  }
+}
