251815f20c1568925f4ae342b616545a05b58e17
AMANI <kingofamani@gmail.com>
2022-12-10 15:32:27 +0800

    修改：UART2

--- BlocklyDuino/WiFi/ESP32S2/02UartConnect/02UartConnectArduinoUNO/02UartConnectArduinoUNO.ino
+++ BlocklyDuino/WiFi/ESP32S2/02UartConnect/02UartConnectArduinoUNO/02UartConnectArduinoUNO.ino
@@ -3,14 +3,12 @@ SoftwareSerial ESP32Serial(3,2);
 
 void setup() {
   Serial.begin(9600);
-  ESP32Serial.begin(4800);
+  ESP32Serial.begin(9600);
 }
 
 void loop() {
   while(ESP32Serial.available()){
-    int val=ESP32Serial.parseInt();
-    if(ESP32Serial.read()=='\n'){
-      Serial.println(val);
-    }
+    String val=ESP32Serial.readString();
+    Serial.println(val);    
   }
 }
--- BlocklyDuino/WiFi/ESP32S2/02UartConnect/02UartConnectESP32S2/02UartConnectESP32S2.ino
+++ BlocklyDuino/WiFi/ESP32S2/02UartConnect/02UartConnectESP32S2/02UartConnectESP32S2.ino
@@ -3,13 +3,14 @@ SoftwareSerial UnoSerial(2,3);
 
 void setup() {
   Serial.begin(9600);
-  UnoSerial.begin(4800);
-  pinMode(2,INPUT);
-  pinMode(3,OUTPUT);
+  UnoSerial.begin(9600);
+//  pinMode(2,INPUT);
+//  pinMode(3,OUTPUT);
 }
 
 void loop() {
-  UnoSerial.print("10");
-  UnoSerial.println("\n");
-  delay(100);
+  //用write，不要用print
+  //不要傳中文字
+  UnoSerial.write("UNO test");
+  delay(1000);
 }
