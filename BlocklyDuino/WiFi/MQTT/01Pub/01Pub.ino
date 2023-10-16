94a9af85c2ae57aff5142c9413ee0dee3f595739
林慶哲 <kingofamani@gmail.com>
2023-10-16 14:35:25 +0800

    新增：MQTT多重訂閱教學

---
+++ BlocklyDuino/WiFi/MQTT/01Pub/01Pub.ino
@@ -0,0 +1,137 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <WiFiClientSecure.h>
+
+//---- WiFi settings
+const char* ssid = "tyes-itc";
+const char* password = "xxxxxx";
+
+//---- HiveMQ Cloud Broker settings
+//HiveMQ Cluster URL
+const char* mqtt_server = "xxxxxx.s2.eu.hivemq.cloud";
+const char* mqtt_username = "kingofamani";
+const char* mqtt_password = "xxxxxx";
+const int mqtt_port = 8883;
+
+const char* TOPIC_MAP = "imiRobot/map/set";
+const char* TOPIC_CAR = "imiRobot/car/standby";
+const char* TOPIC_GOODS = "imiRobot/goods/load";
+
+//發佈者 傳送的消息內容
+char* mqttSendMsg = "";
+//訂閱者 接收的消息內容
+String mqttGetMsg = "";
+
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
+
+// HiveMQ Cloud Let's Encrypt CA certificate
+static const char* root_ca PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----
+)EOF";
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  //randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+//訂閱的主題回覆
+void callback(char* topic, byte* payload, unsigned int length) {
+  mqttGetMsg = "";
+  Serial.print("接收訊息 [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    mqttGetMsg += (char)payload[i];
+  }
+  mqttGetMsg.trim();
+  Serial.println(mqttGetMsg);
+}
+
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection… ");
+    String clientId = "ESP32Client";
+    if (client.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
+      Serial.println("connected!");
+    } else {
+      Serial.print("failed, rc = ");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  delay(500);
+  Serial.begin(9600);
+  delay(500);
+  setup_wifi();
+
+  espClient.setCACert(root_ca);
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+  if (!client.connected()) {
+    reconnect();
+  }
+
+  //發佈
+  mqttSendMsg = "王小明,PS5主機,1,1,3,5";
+  Serial.print("發佈消息:");
+  Serial.println(mqttSendMsg);
+  client.publish(TOPIC_GOODS, mqttSendMsg);
+  Serial.println(String(TOPIC_GOODS)+"已發佈消息成功！請開啟https://console.hivemq.cloud/ 來測試！");
+}
+
+void loop() {  
+  client.loop();
+}
