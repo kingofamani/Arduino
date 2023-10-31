0ea26dd4af5a63c22c0ed92d05a077221327e157
林慶哲 <kingofamani@gmail.com>
2023-10-31 09:40:47 +0800

    新增：麥克納姆輪4路有PWM

--- BlocklyDuino/L298N/Mecanum/02/02.ino
+++ BlocklyDuino/L298N/Mecanum/02/02.ino
@@ -7,10 +7,23 @@
 #define L298N_IN6 4
 #define L298N_IN7 16
 #define L298N_IN8 17
-#define L298N_EN1 //PWM
-#define L298N_EN2 //PWM
-#define L298N_EN3 //PWM
-#define L298N_EN4 //PWM
+
+#define L298N_EN1 13//PWM
+#define L298N_EN2 33//PWM
+#define L298N_EN3 5 //PWM
+#define L298N_EN4 19//PWM
+
+//ESP32設定PWM Channel
+const int EN1_CHL = 0;
+const int EN2_CHL = 1;
+const int EN3_CHL = 2;
+const int EN4_CHL = 3;
+
+//轉速(80~255)
+const int FSpeed = 150;
+const int BSpeed = 150;
+const int RSpeed = 150;
+const int LSpeed = 150;
 
 //轉動時間(毫秒)
 const int FTimer = 1000;
@@ -22,62 +35,93 @@ const int STimer = 3000;
 void forward() {
   digitalWrite(L298N_IN1, HIGH);
   digitalWrite(L298N_IN2, LOW);
+  ledcWrite(EN1_CHL, FSpeed);
+
   digitalWrite(L298N_IN3, HIGH);
   digitalWrite(L298N_IN4, LOW);
+  ledcWrite(EN2_CHL, FSpeed);
 
   digitalWrite(L298N_IN5, HIGH);
   digitalWrite(L298N_IN6, LOW);
+  ledcWrite(EN3_CHL, FSpeed);
+
   digitalWrite(L298N_IN7, HIGH);
   digitalWrite(L298N_IN8, LOW);
+  ledcWrite(EN4_CHL, FSpeed);
 }
 
 void backward() {
   digitalWrite(L298N_IN1, LOW);
   digitalWrite(L298N_IN2, HIGH);
+  ledcWrite(EN1_CHL, BSpeed);
+
   digitalWrite(L298N_IN3, LOW);
   digitalWrite(L298N_IN4, HIGH);
+  ledcWrite(EN2_CHL, BSpeed);
 
   digitalWrite(L298N_IN5, LOW);
   digitalWrite(L298N_IN6, HIGH);
+  ledcWrite(EN3_CHL, BSpeed);
+
   digitalWrite(L298N_IN7, LOW);
   digitalWrite(L298N_IN8, HIGH);
+  ledcWrite(EN4_CHL, BSpeed);
 }
 
 void turnRight() {
   digitalWrite(L298N_IN1, HIGH);
   digitalWrite(L298N_IN2, LOW);
+  ledcWrite(EN1_CHL, RSpeed);
+
   digitalWrite(L298N_IN3, HIGH);
   digitalWrite(L298N_IN4, LOW);
+  ledcWrite(EN2_CHL, RSpeed);
 
   digitalWrite(L298N_IN5, LOW);
   digitalWrite(L298N_IN6, HIGH);
+  ledcWrite(EN3_CHL, RSpeed);
+
   digitalWrite(L298N_IN7, LOW);
   digitalWrite(L298N_IN8, HIGH);
-
+  ledcWrite(EN4_CHL, RSpeed);  
 }
 
 void turnLeft() {
   digitalWrite(L298N_IN1, LOW);
   digitalWrite(L298N_IN2, HIGH);
+  ledcWrite(EN1_CHL, LSpeed);
+
   digitalWrite(L298N_IN3, LOW);
   digitalWrite(L298N_IN4, HIGH);
+  ledcWrite(EN2_CHL, LSpeed);
+
 
   digitalWrite(L298N_IN5, HIGH);
   digitalWrite(L298N_IN6, LOW);
+  ledcWrite(EN3_CHL, LSpeed);
+
   digitalWrite(L298N_IN7, HIGH);
   digitalWrite(L298N_IN8, LOW);
+  ledcWrite(EN4_CHL, LSpeed);
 }
 
 void stopCar() {
   digitalWrite(L298N_IN1, LOW);
   digitalWrite(L298N_IN2, LOW);
+  ledcWrite(EN1_CHL, FSpeed);
+
   digitalWrite(L298N_IN3, LOW);
   digitalWrite(L298N_IN4, LOW);
+  ledcWrite(EN2_CHL, FSpeed);
 
   digitalWrite(L298N_IN5, LOW);
   digitalWrite(L298N_IN6, LOW);
+  ledcWrite(EN3_CHL, FSpeed);
+
   digitalWrite(L298N_IN7, LOW);
   digitalWrite(L298N_IN8, LOW);
+  ledcWrite(EN4_CHL, FSpeed);
+  
 }
 
 void setup() {
@@ -91,6 +135,33 @@ void setup() {
   pinMode(L298N_IN6, OUTPUT);
   pinMode(L298N_IN7, OUTPUT);
   pinMode(L298N_IN8, OUTPUT);
+  pinMode(L298N_EN1, OUTPUT);
+  pinMode(L298N_EN2, OUTPUT);
+  pinMode(L298N_EN3, OUTPUT);
+  pinMode(L298N_EN4, OUTPUT);
+
+  digitalWrite(L298N_IN1, LOW);
+  digitalWrite(L298N_IN2, LOW);
+  digitalWrite(L298N_IN3, LOW);
+  digitalWrite(L298N_IN4, LOW);
+  digitalWrite(L298N_IN5, LOW);
+  digitalWrite(L298N_IN6, LOW);
+  digitalWrite(L298N_IN7, LOW);
+  digitalWrite(L298N_IN8, LOW);
+  digitalWrite(L298N_EN1, LOW);
+  digitalWrite(L298N_EN2, LOW);
+  digitalWrite(L298N_EN3, LOW);
+  digitalWrite(L298N_EN4, LOW);
+
+  //PWM設定
+  ledcSetup(ENA_CHL, 1000, 8);//channel,frequence,resolution(8表示0~255)
+  ledcAttachPin(L298N_EN1, EN1_CHL);
+  ledcSetup(ENB_CHL, 1000, 8);
+  ledcAttachPin(L298N_EN2, EN2_CHL);
+  ledcSetup(ENB_CHL, 1000, 8);
+  ledcAttachPin(L298N_EN3, EN3_CHL);
+  ledcSetup(ENB_CHL, 1000, 8);
+  ledcAttachPin(L298N_EN4, EN4_CHL);
 
   stopCar();
   delay(2000);
