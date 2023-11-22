671e059eb2ca96bcced600c10b854abb182318fc
林慶哲 <kingofamani@gmail.com>
2023-11-22 14:59:22 +0800

    新增：小車模組改4路L298N

--- ArduinoIDE/_Project/SelfDrivingCar/CarModule/07SelfDrivingTrack/Esp32Car/Esp32Car.ino
+++ ArduinoIDE/_Project/SelfDrivingCar/CarModule/07SelfDrivingTrack/Esp32Car/Esp32Car.ino
@@ -27,86 +27,127 @@ char* CAR_MOVE[4] = {"F,", "R,F,", "R,R,F,", "L,F,"};
 //L298N腳位
 #define L298N_IN1 14
 #define L298N_IN2 27
-#define L298N_IN3 16
-#define L298N_IN4 17
-#define L298N_ENA 13//PWM
-#define L298N_ENB 15//PWM
+#define L298N_IN3 26
+#define L298N_IN4 25
+#define L298N_IN5 23
+#define L298N_IN6 4
+#define L298N_IN7 16
+#define L298N_IN8 17
+
+#define L298N_EN1 13//PWM
+#define L298N_EN2 33//PWM
+#define L298N_EN3 5 //PWM
+#define L298N_EN4 19//PWM
 
 //ESP32設定PWM Channel
-const int ENA_CHL = 0;
-const int ENB_CHL = 1;
+const int EN1_CHL = 0;
+const int EN2_CHL = 1;
+const int EN3_CHL = 2;
+const int EN4_CHL = 3;
 
 //轉速(80~255)
-const int FSpeed = 200;
-const int BSpeed = 200;
-const int RSpeed = 150;
-const int LSpeed = 150;
+const int FSpeed = 120;
+const int BSpeed = 120;
+const int RSpeed = 120;
+const int LSpeed = 120;
 
 //轉動時間(毫秒)
-const int FTimer = 500;
-const int BTimer = 500;
+const int FTimer = 1000;
+const int BTimer = 1000;
 const int RTimer = 1000;
 const int LTimer = 1000;
-const int STimer = 1000;
+const int STimer = 3000;
 
 void forward() {
   digitalWrite(L298N_IN1, HIGH);
   digitalWrite(L298N_IN2, LOW);
-  ledcWrite(ENA_CHL, FSpeed);
+  ledcWrite(EN1_CHL, FSpeed);
 
-  digitalWrite(L298N_IN3, LOW);
-  digitalWrite(L298N_IN4, HIGH);
-  ledcWrite(ENB_CHL, FSpeed);
+  digitalWrite(L298N_IN3, HIGH);
+  digitalWrite(L298N_IN4, LOW);
+  ledcWrite(EN2_CHL, FSpeed);
 
-  delay(FTimer);
+  digitalWrite(L298N_IN5, HIGH);
+  digitalWrite(L298N_IN6, LOW);
+  ledcWrite(EN3_CHL, FSpeed);
+
+  digitalWrite(L298N_IN7, HIGH);
+  digitalWrite(L298N_IN8, LOW);
+  ledcWrite(EN4_CHL, FSpeed);
 }
 
 void backward() {
   digitalWrite(L298N_IN1, LOW);
   digitalWrite(L298N_IN2, HIGH);
-  ledcWrite(ENA_CHL, BSpeed);
-
-  digitalWrite(L298N_IN3, HIGH);
-  digitalWrite(L298N_IN4, LOW);
-  ledcWrite(ENB_CHL, BSpeed);
-
-  delay(BTimer);
-}
-
-void turnRight() {
-  digitalWrite(L298N_IN1, LOW);
-  digitalWrite(L298N_IN2, HIGH);
-  ledcWrite(ENA_CHL, LSpeed);
+  ledcWrite(EN1_CHL, BSpeed);
 
   digitalWrite(L298N_IN3, LOW);
   digitalWrite(L298N_IN4, HIGH);
-  ledcWrite(ENB_CHL, LSpeed);
+  ledcWrite(EN2_CHL, BSpeed);
 
-  delay(RTimer);
+  digitalWrite(L298N_IN5, LOW);
+  digitalWrite(L298N_IN6, HIGH);
+  ledcWrite(EN3_CHL, BSpeed);
+
+  digitalWrite(L298N_IN7, LOW);
+  digitalWrite(L298N_IN8, HIGH);
+  ledcWrite(EN4_CHL, BSpeed);
 }
 
-void turnLeft() {
+void turnRight() {
   digitalWrite(L298N_IN1, HIGH);
   digitalWrite(L298N_IN2, LOW);
-  ledcWrite(ENA_CHL, RSpeed);
+  ledcWrite(EN1_CHL, RSpeed);
 
   digitalWrite(L298N_IN3, HIGH);
   digitalWrite(L298N_IN4, LOW);
-  ledcWrite(ENB_CHL, RSpeed);
+  ledcWrite(EN2_CHL, RSpeed);
 
-  delay(LTimer);
+  digitalWrite(L298N_IN5, LOW);
+  digitalWrite(L298N_IN6, HIGH);
+  ledcWrite(EN3_CHL, RSpeed);
+
+  digitalWrite(L298N_IN7, LOW);
+  digitalWrite(L298N_IN8, HIGH);
+  ledcWrite(EN4_CHL, RSpeed);  
+}
+
+void turnLeft() {
+  digitalWrite(L298N_IN1, LOW);
+  digitalWrite(L298N_IN2, HIGH);
+  ledcWrite(EN1_CHL, LSpeed);
+
+  digitalWrite(L298N_IN3, LOW);
+  digitalWrite(L298N_IN4, HIGH);
+  ledcWrite(EN2_CHL, LSpeed);
+
+
+  digitalWrite(L298N_IN5, HIGH);
+  digitalWrite(L298N_IN6, LOW);
+  ledcWrite(EN3_CHL, LSpeed);
+
+  digitalWrite(L298N_IN7, HIGH);
+  digitalWrite(L298N_IN8, LOW);
+  ledcWrite(EN4_CHL, LSpeed);
 }
 
 void stopCar() {
   digitalWrite(L298N_IN1, LOW);
   digitalWrite(L298N_IN2, LOW);
-  ledcWrite(ENA_CHL, FSpeed);
+  ledcWrite(EN1_CHL, FSpeed);
 
   digitalWrite(L298N_IN3, LOW);
   digitalWrite(L298N_IN4, LOW);
-  ledcWrite(ENB_CHL, BSpeed);
+  ledcWrite(EN2_CHL, FSpeed);
 
-  delay(STimer);
+  digitalWrite(L298N_IN5, LOW);
+  digitalWrite(L298N_IN6, LOW);
+  ledcWrite(EN3_CHL, FSpeed);
+
+  digitalWrite(L298N_IN7, LOW);
+  digitalWrite(L298N_IN8, LOW);
+  ledcWrite(EN4_CHL, FSpeed);
+  
 }
 
 void goCar(){
@@ -133,16 +174,22 @@ void goCar(){
   for (int i = 0 ; i < tokenLen ; i++)
   {
     if(pathCarMotor[i]=="F"){
-      forward();      
+      forward();
+      delay(FTimer);
       stopCar();
+      delay(FTimer);
       Serial.print("前,");
     }else if(pathCarMotor[i]=="R"){
       turnRight();
+      delay(RTimer);
       stopCar();
+      delay(RTimer);
       Serial.print("右轉,");
     }else if(pathCarMotor[i]=="L"){
       turnLeft();
+      delay(LTimer);
       stopCar();
+      delay(LTimer);
       Serial.print("左轉,");
     }
   }
@@ -351,29 +398,46 @@ void reverseStringArray(String arr[], int length) {
 void setup() {
   Serial.begin(9600);
   
-  //小車初始化START
+  //=====小車初始化START=====
   pinMode(L298N_IN1, OUTPUT);
   pinMode(L298N_IN2, OUTPUT);
   pinMode(L298N_IN3, OUTPUT);
   pinMode(L298N_IN4, OUTPUT);
-  pinMode(L298N_ENA, OUTPUT);
-  pinMode(L298N_ENB, OUTPUT);
+  pinMode(L298N_IN5, OUTPUT);
+  pinMode(L298N_IN6, OUTPUT);
+  pinMode(L298N_IN7, OUTPUT);
+  pinMode(L298N_IN8, OUTPUT);
+  pinMode(L298N_EN1, OUTPUT);
+  pinMode(L298N_EN2, OUTPUT);
+  pinMode(L298N_EN3, OUTPUT);
+  pinMode(L298N_EN4, OUTPUT);
 
   digitalWrite(L298N_IN1, LOW);
   digitalWrite(L298N_IN2, LOW);
   digitalWrite(L298N_IN3, LOW);
   digitalWrite(L298N_IN4, LOW);
-  digitalWrite(L298N_ENA, LOW);
-  digitalWrite(L298N_ENB, LOW);
+  digitalWrite(L298N_IN5, LOW);
+  digitalWrite(L298N_IN6, LOW);
+  digitalWrite(L298N_IN7, LOW);
+  digitalWrite(L298N_IN8, LOW);
+  digitalWrite(L298N_EN1, LOW);
+  digitalWrite(L298N_EN2, LOW);
+  digitalWrite(L298N_EN3, LOW);
+  digitalWrite(L298N_EN4, LOW);
 
   //PWM設定
-  ledcSetup(ENA_CHL, 1000, 8);//channel,frequence,resolution(8表示0~255)
-  ledcAttachPin(L298N_ENA, ENA_CHL);
-  ledcSetup(ENB_CHL, 1000, 8);
-  ledcAttachPin(L298N_ENB, ENB_CHL);
+  ledcSetup(EN1_CHL, 1000, 8);//channel,frequence,resolution(8表示0~255)
+  ledcAttachPin(L298N_EN1, EN1_CHL);
+  ledcSetup(EN2_CHL, 1000, 8);
+  ledcAttachPin(L298N_EN2, EN2_CHL);
+  ledcSetup(EN3_CHL, 1000, 8);
+  ledcAttachPin(L298N_EN3, EN3_CHL);
+  ledcSetup(EN4_CHL, 1000, 8);
+  ledcAttachPin(L298N_EN4, EN4_CHL);
   
   stopCar();
-  //小車初始化END
+  delay(2000);
+  //=====小車初始化END=====
 
   // 地圖表示，0表示障礙物，1表示可通行
   int grid[numRows][numCols] = {
