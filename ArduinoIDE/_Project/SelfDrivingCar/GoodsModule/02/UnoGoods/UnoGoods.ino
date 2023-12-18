2062589cca30a8410ebdd90f6dcecdfdd95c4585
AMANI <kingofamani@gmail.com>
2023-12-18 21:03:28 +0800

    新增：小車UNO物流模組積木規劃[未完成]

--- ArduinoIDE/_Project/SelfDrivingCar/GoodsModule/02/UnoGoods/UnoGoods.ino
+++ ArduinoIDE/_Project/SelfDrivingCar/GoodsModule/02/UnoGoods/UnoGoods.ino
@@ -1,102 +1,100 @@
-/*
-  Arduino CNC电机扩展板驱动3个NEMA17步进电机示例程序
-  By 太极创客（http://www.taichi-maker.com）
-  2019-03-10
-
-  本示例程序旨在演示如何使用Arduino Uno开发板通过Arduino CNC电机扩展板来驱动3个NEMA17步进电机。
-
-  如需获得更多关于本示例程序的电路连接以及CNC电机扩展板的资料信息，
-  请参考太极创客网站（http://www.taichi-maker.com），并在首页搜索栏中搜索关键字：CNC扩展板
-*/
-#include <AccelStepper.h>  //本示例程序使用AccelStepper库
+#include <AccelStepper.h> 
 #include <Servo.h>
 #include <SoftwareSerial.h>
 
-//伺服馬達
+//====插入變數Start====
+// 定義電機控制用常數
+const int enablePin = 8;  // 啟用控制引腳
+const int xdirPin = 5;     // x方向控制引腳
+const int xstepPin = 2;    // x步進控制引腳
+const int ydirPin = 6;     // y方向控制引腳
+const int ystepPin = 3;    // y步進控制引腳
+
+// UART通訊
+SoftwareSerial ESP32Serial(19, 18);
+
+// 伺服馬達腳位（接在Z+是pin11）
+const int servoPin = 11;   
+//====插入變數End====
+
+// 伺服馬達
 Servo servoPick;
 
-// 定义电机控制用常量
-const int enablePin = 8;  // 使能控制引脚
+const int moveSteps = 500;    // 測試電機運行使用的運行步數
 
-const int xdirPin = 5;     // x方向控制引脚
-const int xstepPin = 2;    // x步进控制引脚
-const int ydirPin = 6;     // y方向控制引脚
-const int ystepPin = 3;    // y步进控制引脚
+AccelStepper stepper1(1, xstepPin, xdirPin); // 建立步進電機物件1
+AccelStepper stepper2(1, ystepPin, ydirPin); // 建立步進電機物件2
 
-const int servoPin = 11;   //伺服馬達腳位(接在Z+是pin11)
-
-const int moveSteps = 500;    //测试电机运行使用的运行步数
-
-AccelStepper stepper1(1, xstepPin, xdirPin); //建立步进电机对象1
-AccelStepper stepper2(1, ystepPin, ydirPin); //建立步进电机对象2
-
-//====UART通訊(接收ESP32傳來的MQTT訊息) start====
-SoftwareSerial ESP32Serial(19, 18);
 const char* GOODS_LOAD = "goodsLoad";
 
 void setup() {	
   Serial.begin(9600);
-  //UART
+  // UART
   ESP32Serial.begin(9600);
 
-  //伺服馬達
+  // 伺服馬達
   servoPick.attach(servoPin);
   servoPick.write(0);
 
-  pinMode(xstepPin, OUTPUT);    // Arduino控制A4988x步进引脚为输出模式
-  pinMode(xdirPin, OUTPUT);     // Arduino控制A4988x方向引脚为输出模式
-  pinMode(ystepPin, OUTPUT);    // Arduino控制A4988y步进引脚为输出模式
-  pinMode(ydirPin, OUTPUT);     // Arduino控制A4988y方向引脚为输出模式
+  pinMode(xstepPin, OUTPUT);    // Arduino控制A4988x步進引腳為輸出模式
+  pinMode(xdirPin, OUTPUT);     // Arduino控制A4988x方向引腳為輸出模式
+  pinMode(ystepPin, OUTPUT);    // Arduino控制A4988y步進引腳為輸出模式
+  pinMode(ydirPin, OUTPUT);     // Arduino控制A4988y方向引腳為輸出模式
 
-  pinMode(enablePin, OUTPUT);  // Arduino控制A4988使能引脚为输出模式
-  digitalWrite(enablePin, LOW); // 将使能控制引脚设置为低电平从而让电机驱动板进入工作状态
+  pinMode(enablePin, OUTPUT);  // Arduino控制A4988啟用引腳為輸出模式
+  digitalWrite(enablePin, LOW); // 將啟用控制引腳設置為低電平以使電機驅動板進入工作狀態
 
-  stepper1.setMaxSpeed(300.0);     // 设置电机最大速度300
-  stepper1.setAcceleration(20.0);  // 设置电机加速度20.0
-  stepper2.setMaxSpeed(300.0);     // 设置电机最大速度300
-  stepper2.setAcceleration(20.0);  // 设置电机加速度20.0
+  stepper1.setMaxSpeed(300.0);     // 設置電機最大速度300
+  stepper1.setAcceleration(20.0);  // 設置電機加速度20.0
+  stepper2.setMaxSpeed(300.0);     // 設置電機最大速度300
+  stepper2.setAcceleration(20.0);  // 設置電機加速度20.0
 }
 
 void loop() {
-	//傳送訊息：Arduino→ESP32
-  if (Serial.available()) {
-    String str = Serial.readString();
-    ESP32Serial.print(str);
-  }
-  //接收訊息：ESP32→Arduino
+  // 接收訊息：ESP32→Arduino
   while (ESP32Serial.available()) {
-    String str = ESP32Serial.readString();//格式xy(例如13)
-	//Serial.println(str);   
+	//取得貨物在倉庫xy座標
+    String str = ESP32Serial.readString();// 格式xy(例如13)
 	int x = (int)(str.charAt(0) - '0');
 	int y = (int)(str.charAt(1) - '0');
-    //開始移動CNC
 	
-	//推動貨物
+    //CNC開始依座標找尋貨物
+	findGoods(x,y);
 	
-	//完成撿貨
+	// 撿貨伺服馬達
+	servoPick.write(180);
+	delay(2000);
+	servoPick.write(0);
+	delay(2000);
+	
+	// 完成撿貨通知ESP32
 	ESP32Serial.print(GOODS_LOAD);
 	
   }  
   
-}//end loop
+}// end loop
+
+void findGoods(int x,int y){
+	//...
+}
 
 void testStepper(){
-	// 控制步进电机1往复运动
+	// 控制步進電機1往復運動
   if ( stepper1.currentPosition() == 0 ) {
     stepper1.moveTo(moveSteps);
-    servoPick.write(0);//伺服馬達轉0度
+    servoPick.write(0);// 伺服馬達轉0度
   } else if ( stepper1.currentPosition() == moveSteps  ) {
     stepper1.moveTo(0);
-    servoPick.write(90);//伺服馬達轉90度
+    servoPick.write(90);// 伺服馬達轉90度
   }
 
-  // 控制步进电机2往复运动
+  // 控制步進電機2往復運動
   if ( stepper2.currentPosition() == 0 ) {
     stepper2.moveTo(moveSteps);
   } else if ( stepper2.currentPosition() == moveSteps  ) {
     stepper2.moveTo(0);
   }
 
-  stepper1.run();   // 1号电机运行
-  stepper2.run();   // 2号电机运行
+  stepper1.run();   // 1號電機運行
+  stepper2.run();   // 2號電機運行
 }
