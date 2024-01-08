cb329e2a224d29825ca806dd85c7e107333a4f2d
林慶哲 <kingofamani@gmail.com>
2024-01-08 15:49:57 +0800

    新增：4x4鍵盤(I2C)的export資料夾

---
+++ tools/myCustomBlockly/amani/I2C鍵盤/export/javascript.js
@@ -0,0 +1,38 @@
+javascript.javascriptGenerator.forBlock['_01amani_i2ckeypad_init'] = function(block, generator) {
+  // TODO: Assemble javascript into code variable.
+  var code = '...\n';
+  return code;
+};
+
+javascript.javascriptGenerator.forBlock['_02amani_i2ckeypad_loop'] = function(block, generator) {
+  // TODO: Assemble javascript into code variable.
+  var code = '...\n';
+  return code;
+};
+
+javascript.javascriptGenerator.forBlock['_03amani_i2ckeypad_iskey'] = function(block, generator) {
+  // TODO: Assemble javascript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.javascript.ORDER_NONE];
+};
+
+javascript.javascriptGenerator.forBlock['_04amani_i2ckeypad_key'] = function(block, generator) {
+  // TODO: Assemble javascript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.javascript.ORDER_NONE];
+};
+
+javascript.javascriptGenerator.forBlock['_05amani_i2ckeypad_keynull'] = function(block, generator) {
+  // TODO: Assemble javascript into code variable.
+  var code = '...\n';
+  return code;
+};
+
+javascript.javascriptGenerator.forBlock['_06amani_i2ckeypad_funcnum'] = function(block, generator) {
+  var statements_msg = generator.statementToCode(block, 'msg');
+  // TODO: Assemble javascript into code variable.
+  var code = '...\n';
+  return code;
+};
