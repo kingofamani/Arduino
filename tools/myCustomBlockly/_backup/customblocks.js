140c8c870e2fe85f2d2b77cd22eef50aadd7e29c
AMANI <kingofamani@gmail.com>
2024-05-05 12:07:14 +0800

    新增：自訂積木放在GitHub Pages

---
+++ tools/myCustomBlockly/_backup/customblocks.js
@@ -0,0 +1,31 @@
+var customBlocks = [
+["https://kingofamani.github.io/BlocklyExt/extensions/","category_servo"],
+
+["https://circuspi.github.io/ICSHOP/","category_servo"],
+["https://lungchu.github.io/blockly/extensions/","category_servo"],
+["https://lioujj.github.io/LinkIt7697/extensions/","category_servo"],
+["https://fustyles.github.io/webduino/LinkIt7697/France/","category_servo"]
+];
+
+/*
+["customBlocks/amaniBlock/","category_servo"],
+["customBlocks/amaniBlock/basicBlock/","category_ifttt"],
+["customBlocks/amaniBlock/i2cKeypadBlock/","category_ifttt"],
+["customBlocks/amaniBlock/simpleTimerBlock/","category_ifttt"],
+["customBlocks/imiBlock/","category_servo"],
+["customBlocks/imiBlock/espCarBlock/","category_ifttt"],
+["customBlocks/imiBlock/megaCarBlock/","category_ifttt"],
+["customBlocks/imiBlock/espGoodsBlock/","category_ifttt"],
+["customBlocks/imiBlock/unoGoodsBlock/","category_ifttt"],
+["customBlocks/imiBlock/mqttCloudBlock/","category_ifttt"],
+["customBlocks/imiBlock/gpsCloudBlock/","category_ifttt"],
+["customBlocks/imiBlock/esp32mapBlock/","category_ifttt"],
+["customBlocks/imiBlock/espChargeBlock/","category_ifttt"],
+
+var customBlocks = [
+["https://circuspi.github.io/ICSHOP/","category_external"],
+["https://lioujj.github.io/LinkIt7697/extensions/","category_external"],
+["customBlocks/France/",""]
+];
+
+*/
